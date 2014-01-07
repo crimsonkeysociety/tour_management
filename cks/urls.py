a48a4bd8254a5f3955d6f861bc9644ef8b4296ed
Crimson Key Society <crimsonkeysociety@gmail.com>
2014-01-07 15:32:09 -0500

    admin urls fix

--- cks/urls.py
+++ cks/urls.py
@@ -2,6 +2,8 @@ from django.conf.urls import patterns, include, url
 
 from django.contrib import admin
 
+admin.autodiscover()
+
 urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
     url(r'^$', 'app.views.home', name='home-url'),
@@ -35,4 +37,3 @@ urlpatterns = patterns('',
     url(r'^logout/$', 'app.views.logout', name='logout-url'),
     url(r'', include('social.apps.django_app.urls', namespace='social')),
 )
-admin.autodiscover()
