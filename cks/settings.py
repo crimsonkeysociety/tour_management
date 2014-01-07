093ab7b156962ee56d1c3d29849ca81b7506174e
Crimson Key Society <crimsonkeysociety@gmail.com>
2014-01-07 15:46:42 -0500

    admin urls

--- cks/settings.py
+++ cks/settings.py
@@ -23,7 +23,7 @@ SECRET_KEY = private_settings.SECRET_KEY
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-TEMPLATE_DEBUG = False
+TEMPLATE_DEBUG = True
 
 
 # Application definition
