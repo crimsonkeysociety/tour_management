e0edca367747723cafc86ca0342a6647435f88c4
Andrew Raftery <andrewraftery@Andrews-MacBook-Pro.local>
2014-01-19 17:33:08 -0500

    heroku static files

--- cks/settings_heroku.py
+++ cks/settings_heroku.py
@@ -10,7 +10,7 @@ DEFAULT_FILE_STORAGE = 'libs.storages.S3Storage.S3Storage'
 AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY')
 AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
 AWS_STORAGE_BUCKET_NAME = 'cks'
-STATIC_URL = '{}.s3.amazonaws.com/'.format(AWS_STORAGE_BUCKET_NAME)
+STATIC_URL = 'http://{}.s3.amazonaws.com/'.format(AWS_STORAGE_BUCKET_NAME)
 
 
 DBBACKUP_STORAGE = 'dbbackup.storage.s3_storage'
