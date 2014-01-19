4e4174af40f8fc9757329f0c2184790d2fe3914a
Andrew Raftery <andrewraftery@Andrews-MacBook-Pro.local>
2014-01-19 17:28:50 -0500

    heroku static files

--- cks/settings_heroku.py
+++ cks/settings_heroku.py
@@ -5,9 +5,18 @@ import os, os.path
 ##### STATIC FILES #########################################
 ############################################################
 
-# Static asset configuration
-STATIC_ROOT = 'staticfiles'
+STATICFILES_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
+DEFAULT_FILE_STORAGE = 'libs.storages.S3Storage.S3Storage'
 STATIC_URL = '/static/'
+AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY')
+AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
+AWS_STORAGE_BUCKET_NAME = 'cks'
+
+
+DBBACKUP_STORAGE = 'dbbackup.storage.s3_storage'
+DBBACKUP_S3_BUCKET = 'cks_db_backups'
+DBBACKUP_S3_ACCESS_KEY = AWS_ACCESS_KEY_ID
+DBBACKUP_S3_SECRET_KEY = AWS_SECRET_ACCESS_KEY
 
 ############################################################
 ##### DATABASE #############################################
