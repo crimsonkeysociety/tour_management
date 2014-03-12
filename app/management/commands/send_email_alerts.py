9053bbf18a40c1c51e9c00a1239da8c03f89407f
Andrew Raftery <andrewraftery@Andrews-MacBook-Pro-2.local>
2014-03-12 17:50:20 -0400

    shift emails

--- app/management/commands/send_email_alerts.py
+++ app/management/commands/send_email_alerts.py
@@ -1,5 +1,5 @@
 from django.core.management.base import BaseCommand, CommandError
-from app.models import Tour
+from app.models import Tour, Shift
 from app import reminder_utilities
 import pytz
 from django.conf import settings
@@ -12,11 +12,11 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         """
-        Sends text alerts for tours occurring tomorrow.
+        Sends text alerts for tours and shifts occurring tomorrow.
         """
         tomorrow = timezone.now().astimezone(pytz.timezone(settings.TIME_ZONE)) + datetime.timedelta(days=1)
 
-        # get all of today's tours
+        # get all of tomorrow's tours
         tours = Tour.objects.filter(time__day=tomorrow.day, time__month=tomorrow.month, time__year=tomorrow.year)
         emails_sent = 0
         for tour in tours:
@@ -24,4 +24,14 @@ class Command(BaseCommand):
                 reminder_utilities.send_email(tour)
                 emails_sent += 1
 
-        self.stdout.write('Sent {} emails successfully.'.format(emails_sent))
+        self.stdout.write('Sent {} tour emails successfully.'.format(emails_sent))
+
+        # get all of tomorrow's shifts
+        shifts = Shift.objects.filter(time__day=tomorrow.day, time__month=tomorrow.month, time__year=tomorrow.year)
+        emails_sent = 0
+        for shift in shifts:
+            if shift.person:
+                reminder_utilities.send_shift_email(shift)
+                emails_sent += 1
+
+        self.stdout.write('Sent {} shift emails successfully.'.format(emails_sent))
--- app/reminder_utilities.py
+++ app/reminder_utilities.py
@@ -52,6 +52,30 @@ def send_email(tour):
 	msg.attach_alternative(html_content, "text/html")
 	msg.send()
 
+def send_shift_email(shift):
+	plaintext = get_template('email/shift_reminder.txt')
+	htmly     = get_template('email/shift_reminder.html')
+	d = Context({ 'shift': shift })
+	subject = 'Shift Tomorrow at {0}'.format(shift.time.astimezone(pytz.timezone(settings.TIME_ZONE)).strftime('%I:%M %p'))
+	to = shift.person.email
+
+	reply_to = models.Person.objects.filter(position='Tour Coordinator').exclude(position='Tour Coordinator (Primary)').first()
+	if not reply_to:
+		reply_to = models.Person.objects.filter(position='Tour Coordinator').first()
+
+	if reply_to:
+		reply_to_email = reply_to.email
+	else:
+		reply_to_email = 'crimsonkeysociety@gmail.com'
+
+	from_email = reply_to_email
+
+	text_content = plaintext.render(d)
+	html_content = htmly.render(d)
+	msg = EmailMultiAlternatives(subject, text_content, from_email, [to], headers={'Reply-To': reply_to_email })
+	msg.attach_alternative(html_content, "text/html")
+	msg.send()
+
 def send_requirements_email(person):
 	now = timezone.now()
 	semester = utilities.current_semester()
